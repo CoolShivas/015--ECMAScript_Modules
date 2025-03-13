55c4d7895891203be0fe6635d4a4185c8081b2f0
CoolShivas <shivas2710cool00@gmail.com>
2025-03-13 19:59:48 +0530

    Named Export way of ES Modules.

--- assign/firstFile.js
+++ assign/firstFile.js
@@ -1,26 +1,26 @@
 // // // // Commonjs way of Doing the stuff:-
 
-const addition = (a, b) => {
-    return (a + b);
-};
+// const addition = (a, b) => {
+//     return (a + b);
+// };
 
-const subtraction = (a, b) => {
-    return (a - b);
-};
+// const subtraction = (a, b) => {
+//     return (a - b);
+// };
 
-const multiply = (a, b) => {
-    return (a * b);
-};
+// const multiply = (a, b) => {
+//     return (a * b);
+// };
 
-const division = (a, b) => {
-    return (a / b);
-};
+// const division = (a, b) => {
+//     return (a / b);
+// };
 
 
-module.exports.addition = addition;
-module.exports.subtraction = subtraction;
-module.exports.multiply = multiply;
-module.exports.division = division;
+// module.exports.addition = addition;
+// module.exports.subtraction = subtraction;
+// module.exports.multiply = multiply;
+// module.exports.division = division;
 
 
 // // // // Commonjs way of exports using property.
@@ -28,3 +28,27 @@ module.exports.division = division;
 //////////************************************************************************************ */
 //////////---------------------------------------------------------------------------------------
 //////////************************************************************************************ */
+
+
+
+
+// // // // ES Modules (ECMAScript Modules) way of Doing the same above stuff:-
+
+
+// // // Named Export way of ES Modules :-
+
+export const addition = (a, b) => {
+    return (a + b);
+};
+
+export const subtraction = (a, b) => {
+    return (a - b);
+};
+
+export const multiply = (a, b) => {
+    return (a * b);
+};
+
+export const division = (a, b) => {
+    return (a / b);
+};
--- assign/secondFile.js
+++ assign/secondFile.js
@@ -16,17 +16,35 @@
 /////////---------------------------------------------------------------------------------------
 
 
-const secFileFunc = require('./firstFile');
+// const secFileFunc = require('./firstFile');
 
-console.log(secFileFunc.addition(5,5));     // 10
-console.log(secFileFunc.subtraction(5,5));  // 0
-console.log(secFileFunc.multiply(50,5));     // 250
-console.log(secFileFunc.division(5,5));     // 1
+// console.log(secFileFunc.addition(5,5));     // 10
+// console.log(secFileFunc.subtraction(5,5));  // 0
+// console.log(secFileFunc.multiply(50,5));     // 250
+// console.log(secFileFunc.division(5,5));     // 1
 
 // // // // Instead of using node secondFile.js again and again for the result.
 // // // // Using node --watch secondFile.js It will run automatically if any thing updated.
 
 
+
+
 //////////************************************************************************************ */
 //////////---------------------------------------------------------------------------------------
 //////////************************************************************************************ */
+
+
+
+// // // // ES Modules (ECMAScript Modules) way of Doing the same above stuff:-
+
+
+// // // Named Export way of ES Modules :-
+
+
+import { addition, subtraction, division, multiply } from './firstFile.js';
+
+
+console.log(addition(50,4));        // 54
+console.log(subtraction(50,4));     // 46
+console.log(division(50,4));        // 12.5
+console.log(multiply(50,4));        // 200
