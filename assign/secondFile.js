f37071c8e65b8dc123f8645ea947248c2504f4a2
CoolShivas <shivas2710cool00@gmail.com>
2025-03-13 18:00:26 +0530

    Commonjs way of Doing the stuff

---
+++ assign/secondFile.js
@@ -0,0 +1,19 @@
+// // // // Commonjs way of Doing the stuff:-
+
+const addFunc = require('./firstFile');
+
+console.log(addFunc(5,7));
+
+// // // // Here, we are getting the Output on Terminal as :-
+// // // // Step.1) Open the terminal and clear all the stuff.
+// // // // Step.2) Then, check the list (i.e., by command ls).  
+// // // // Step.3) Then, move inside the folder (i.e., by command cd).  
+// // // // Step.4) Then, check the file in which you want to run (i.e., by command ls).  
+// // // // Step.5) Finally, choose the file ((i.e., by command node secondFile.js) for the result.
+// // // // Therefore, you get the result as 12;
+
+
+
+//////////************************************************************************************ */
+//////////---------------------------------------------------------------------------------------
+//////////************************************************************************************ */
